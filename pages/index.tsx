d3f8b76c90b306d886c99739fa31b48e4a2be838
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-11 13:46:41 +0900

    tailwind css setup and configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,5 +4,9 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// 어느 컴포넌트(혹은 page)에 tailwind를 사용할 것인지 설정
