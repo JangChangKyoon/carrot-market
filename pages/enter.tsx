b92df9be1a0bae131693dcb5182c3d9ae825b1b5
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-14 22:29:30 +0900

    Update enter.tsx

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -11,7 +11,7 @@ interface EnterForm {
 }
 
 export default function Enter() {
-  const { register, handleSubmit, reset } = useForm<EnterForm>();
+  const { register, handleSubmit, reset, watch } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
     reset();
@@ -24,6 +24,7 @@ export default function Enter() {
   const onValid = (data: EnterForm) => {
     console.log(data);
   };
+  // console.log(watch());
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
