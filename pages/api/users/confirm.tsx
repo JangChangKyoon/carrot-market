0036fb51922649d264c93fe348f778af16f57c5e
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-20 22:51:11 +0900

    #9.8 Profile Handler (05:18)

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -3,24 +3,32 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withIronSessionApiRoute } from "iron-session/next";
 
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
   // console.log(token);
-  console.log(req.session);
+  // console.log(req.session);
   const exists = await client.token.findUnique({
     where: {
       payload: token,
     },
     // include: { user: true },
   });
-  if (!exists) res.status(404).end();
+  if (!exists) return res.status(404).end();
   req.session.user = {
-    id: exists?.userId,
+    id: exists.userId,
   };
-  console.log(exists);
+  // console.log(exists);
   await req.session.save();
   res.status(200).end();
 }
