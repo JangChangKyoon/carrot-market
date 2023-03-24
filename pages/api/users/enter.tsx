eff759bf90e799cdb8633bf79e49318955b0bc15
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-24 18:18:30 +0900

    #10.1 Protected Handlers (09:05)

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -110,4 +110,4 @@ async function handler(
   return res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withHandler({ method: "POST", handler, isPrivate: false });
