fe81fef2fb45e24643bdd26d888cc49008cae765
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-27 01:10:42 +0900

    #11.10 Counting Relationships (05:00) : 목록화면에서 상품별 좋아요 수 계산하여 표시

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,19 @@ import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
 import Head from "next/head";
-import useUser from "@libs/client/userUser";
+import useUser from "@libs/client/useUser";
 import useSWR from "swr";
 import { Product } from "@prisma/client";
 
+interface ProductWithCount extends Product {
+  _count: {
+    favs: number;
+  };
+}
+
 interface ProductsResponse {
   ok: boolean;
-  products: Product[];
+  products: ProductWithCount[];
 }
 
 const Home: NextPage = () => {
@@ -29,8 +35,7 @@ const Home: NextPage = () => {
             key={product.id}
             title={product.name}
             price={product.price}
-            comments={1}
-            hearts={1}
+            hearts={product._count.favs}
           />
         ))}
         <FloatingButton href="/products/upload">
